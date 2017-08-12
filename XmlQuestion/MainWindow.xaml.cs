8363a28a1652bdc184e9cee619d5a91245fe604d
arch <arthurshcherbakov@gmail.com>
2017-08-12 16:35:41 +0700

    First work version programm. Add graphic interface

--- XmlQuestion/MainWindow.xaml.cs
+++ XmlQuestion/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,25 +21,71 @@ namespace XmlQuestion
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<ProcessingElement> ListProcessingElement = new List<ProcessingElement>();
+        string pathForSaveXMLFiles;
+
+
         public MainWindow()
         {
             InitializeComponent();
 
-            ParseExel excel = new ParseExel(@"D:\ExampleExcel.xlsx", 1);
+            //ParseExel excel = new ParseExel(@"D:\ExampleExcel.xlsx", 1);
 
-            excel.StartParse();
+            //excel.StartParse();
 
-            IdentificationOfTypeQuestion uhhjujuh = new IdentificationOfTypeQuestion(excel.questions);
+            //IdentificationOfTypeQuestion uhhjujuh = new IdentificationOfTypeQuestion(excel.questions);
 
-            WriterXMLQuestion dwdwdsawd = new WriterXMLQuestion(uhhjujuh.ListQuestionWithType, excel.categoryName, excel.questionName, "users.xml");
+            //WriterXMLQuestion dwdwdsawd = new WriterXMLQuestion(uhhjujuh.ListQuestionWithType, excel.categoryName, excel.questionName, "users.xml");
 
+           
+            
             //ParseExel excel1232 = new ParseExel(@"D:\ExampleExcel.xlsx", 1);
-
+            
 
 
             //string sdasdawdwad = excel.ReadCell(1, 1);
 
 
         }
+
+        private void SelectFileForParse(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                ListProcessingElement.Clear();
+                ListProcessingElement.Add(new ProcessingElement(openFileDialog.FileName));
+                //openFileDialog.filed
+
+            }
+            
+            //using (var fbd = new FolderBrowserDialog())
+            //{
+            //    DialogResult result = fbd.ShowDialog();
+
+            //    if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            //    {
+            //        string[] files = Directory.GetFiles(fbd.SelectedPath);
+
+            //        System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+            //    }
+            //}
+        }
+
+        private void StartParsing(object sender, RoutedEventArgs e)
+        {
+            foreach (var el in ListProcessingElement)
+            {
+                el.Go("user.xml");
+            }
+        }
+
+        //Start Parsing
     }
 }
---
+++ XmlQuestion/ProcessingElement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlQuestion
+{
+    class ProcessingElement
+    {
+        ParseExel ParseExel;
+        IdentificationOfTypeQuestion IoTQ;
+        WriterXMLQuestion WXMLQ;
+
+        public ProcessingElement(string path)
+        {
+            ParseExel = new ParseExel(path,1);
+        }
+
+        public void Go(string pathToSave)
+        {
+            ParseExel.StartParse();
+            IoTQ = new IdentificationOfTypeQuestion(ParseExel.questions);
+
+            WXMLQ = new WriterXMLQuestion(IoTQ.ListQuestionWithType, ParseExel.categoryName, ParseExel.questionName, pathToSave);
+
+        }
+    }
+}
